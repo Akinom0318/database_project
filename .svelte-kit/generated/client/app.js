a64cedcb2f07c221e7331a3ef47c16975661b67c
SunGj921028 <sungj921028@gmail.com>
2024-05-26 22:47:38 +0800

    TabGroup adjustment!!

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -6,16 +6,18 @@ export const nodes = [
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [~2],
-		"/login": [3],
-		"/register": [4],
-		"/test": [5]
+		"/admin": [3],
+		"/login": [4],
+		"/register": [5],
+		"/test": [6]
 	};
 
 export const hooks = {
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import MessageModal from '../MessageModal.svelte';
+    //! Message Showing
+    let modalMessage = '';
+    let modalVisible = false;
+    function showModal(message: string) {
+        modalMessage = message;
+        modalVisible = true;
+    }
+</script>
+
+<style>
+
+</style>
+
+<!-- Message Box -->
+<MessageModal bind:visible={modalVisible} bind:message={modalMessage} />
+
+<div></div>
+
