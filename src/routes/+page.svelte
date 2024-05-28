b7f6b7790b2071213ebd31fdfac9593ca046de40
Akinom <57698715+Akinom0318@users.noreply.github.com>
2024-05-28 17:58:44 +0800

    Homepage renovation v1.0 (#13)
    * add user_UI page
    
    * finish basic user info modification
    
    * fix some bugs with new schema
    
    * Renovation the Homepage :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { fly } from "svelte/transition";
     import { current_account } from "../store.js";
-    import Datatable from "./Datatable.svelte";
+    import ProductTable from "./ProductTable.svelte";
 
     export let data;
 
@@ -12,14 +12,38 @@
 </script>
 
 <style>
-    #datatable-container {
+    #ProductTable-container {
         padding-top: 15px;
     }
+
+    .login-detection{
+        text-align: center;
+        margin: 50px;
+    }
+    
+    #rickroll{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+
+    }
+
 </style>
 
 <!-- <p in:fly={{ y: 20 }} >Home page!</p> -->
 {#if local_current_account}
-    <div id="datatable-container">
-        <Datatable data={data.products} />
+    <div id="ProductTable-container">
+        <ProductTable data={data.products} />
+    </div>
+{:else}
+    <div class="login-detection">
+        <aside class="alert variant-ghost-tertiary" in:fly={{ y: 20 }}>
+            <div class="alert-message">
+                <h2 class="h2">
+                    Oh no! It seems you haven't log in!
+                </h2>
+                <img src="src\lib\images\rickroll.png" id="rickroll" alt="Get RickRoll!" width="30%" height="30%">
+            </div>
+        </aside>        
     </div>
 {/if}
