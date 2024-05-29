75271d33203cc370298b20745f310c43f9040055
SunGj921028 <111631555+SunGj921028@users.noreply.github.com>
2024-05-29 23:30:06 +0800

    Add the admin page. (#14)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -66,46 +66,87 @@
 </style>
 
 <!-- hover="hover:variant-soft-primary" -->
-<TabGroup 
-	active="variant-filled-primary"
-	rounded=10px
-	border=10px
-	flex="flex-1 lg:flex-none"
-	class="bg-surface-100-800-token w-full items-center justify-between font-bold">
-	<div id="left-container" class="flex flex-1 space-x-3 items-center justify-start">
-		<TabAnchor href="/" class="text-center">
-			<svelte:fragment slot="lead"><span id="home-icon" style="color: cadetblue">&#9750;</span> Home</svelte:fragment>
-		</TabAnchor>
-		{#if !local_current_account}
-			<TabAnchor href="/login" class="text-center">
-				<svelte:fragment slot="lead"><span>&#10070;</span> Login</svelte:fragment>
+{#if local_current_account != "admin"}
+	<TabGroup 
+		active="variant-filled-primary"
+		rounded=10px
+		border=10px
+		flex="flex-1 lg:flex-none"
+		class="bg-surface-100-800-token w-full items-center justify-between font-bold">
+		<div id="left-container" class="flex flex-1 space-x-3 items-center justify-start">
+			<TabAnchor href="/" class="text-center">
+				<svelte:fragment slot="lead"><span id="home-icon" style="color: cadetblue">&#9750;</span> Home</svelte:fragment>
 			</TabAnchor>
+			{#if !local_current_account}
+				<TabAnchor href="/login" class="text-center">
+					<svelte:fragment slot="lead"><span>&#10070;</span> Login</svelte:fragment>
+				</TabAnchor>
+			{/if}
+				<TabAnchor href="/register" class="text-center">
+					<svelte:fragment slot="lead"><span>&#9997;</span> Register</svelte:fragment>
+				</TabAnchor>
+		</div>
+		
+		{#if local_current_account}
+			<div id="welcome-container" class="flex items-center">
+				<a href="/user_UI">
+					<p id="welcome-message" class="text-center">
+						<span style="color: red;">&#9733;</span> Welcome, {local_current_account}!
+					</p>
+				</a>
+			</div>
+			<div id="right-container" class="flex space-x-3 items-center justify-end">
+				<div id="light-switch">
+					<LightSwitch />
+				</div>
+				<a href="../login">
+					<button on:click={log_out}
+						id = "Log_out_button"
+						class="btn variant-filled"
+						type="button">Log out
+					</button>
+				</a>
+			</div>
 		{/if}
-			<TabAnchor href="/register" class="text-center">
-				<svelte:fragment slot="lead"><span>&#9997;</span> Register</svelte:fragment>
+	</TabGroup>
+{:else}
+	<TabGroup 
+		active="variant-filled-primary"
+		rounded=10px
+		border=10px
+		flex="flex-1 lg:flex-none"
+		class="bg-surface-100-800-token w-full items-center justify-between font-bold">
+		<div id="left-container" class="flex flex-1 space-x-3 items-center justify-start">
+			<TabAnchor href="/user_UI/admin/product" class="text-center">
+				<svelte:fragment slot="lead"><span id="home-icon" style="color: cadetblue">&#9752;</span> product list</svelte:fragment>
 			</TabAnchor>
-	</div>
-	
-	{#if local_current_account}
+			<TabAnchor href="/user_UI/admin/userList" class="text-center">
+				<svelte:fragment slot="lead"><span style="color:darkslateblue;">&#9738;</span> user list</svelte:fragment>
+			</TabAnchor>
+			<TabAnchor href="/user_UI/admin/transaction" class="text-center">
+				<svelte:fragment slot="lead"><span>&#8471;</span> transaction list</svelte:fragment>
+			</TabAnchor>
+		</div>
+
 		<div id="welcome-container" class="flex items-center">
-			<a href="/user_UI">
-				<p id="welcome-message" class="text-center">
-					<span style="color: red;">&#9733;</span> Welcome, {local_current_account}!
-				</p>				
-			</a>
+			<p id="welcome-message" class="text-center">
+				Hello <span style="color: red;">&#10084;</span> , {local_current_account}!
+			</p>
 		</div>
 		<div id="right-container" class="flex space-x-3 items-center justify-end">
 			<div id="light-switch">
 				<LightSwitch />
 			</div>
-			<button on:click={log_out}
-				id = "Log_out_button"
-				class="btn variant-filled"
-				type="button">Log out
-			</button>
+			<a href="../../login">
+				<button on:click={log_out}
+					id = "Log_out_button"
+					class="btn variant-filled"
+					type="button">Log out
+				</button>
+			</a>
 		</div>
-	{/if}
-</TabGroup>
+	</TabGroup>
+{/if}
 
 <!--TODO I don't know what to do, so i comment it. -->
 <!-- {#if local_current_account}
---
+++ src/routes/user_UI/admin/product/adminProductTable.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import Pagination from '$lib/product_table/Pagination.svelte';
+	import RowCount from '$lib/product_table/RowCount.svelte';
+	import RowsPerPage from '$lib/product_table/RowsPerPage.svelte';
+	import Search from '$lib/product_table/Search.svelte';
+	import ThSort from '$lib/product_table/ThSort.svelte';
+	import { DataHandler } from '@vincjo/datatables';
+
+	export let data: any[];
+    //Init data handler - SERVER
+	const handler = new DataHandler(data, { rowsPerPage: 5 });
+	const rows = handler.getRows();
+
+</script>
+
+<style>
+	#product-container {
+		padding: 0.8rem;
+		font-size: 1.05rem;
+	}
+
+	#product-table {
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	.table tbody td {
+		font-size: 0.97rem;
+	}
+
+</style>
+
+
+<div id="product-container" class="overflow-y-auto space-y-4">
+	<header class="flex justify-between">
+		<Search {handler} />
+		<RowsPerPage {handler} />
+	</header>
+	<table id="product-table" class="table table-hover table-compact w-full table-auto">
+		<thead>
+			<tr>
+				<ThSort {handler} orderBy="product_name">Product Name</ThSort>
+				<ThSort {handler} orderBy="selling_price">Price</ThSort>
+				<ThSort {handler} orderBy="sales">Sales</ThSort>
+				<ThSort {handler} orderBy="likes">Likes</ThSort>
+				<ThSort {handler} orderBy="avg_score">Average Score</ThSort>
+				<ThSort {handler} orderBy="tags">Tags</ThSort>
+			</tr>
+		</thead>
+		<tbody id="content" style="text-align: center;">
+			<!-- display each product as following order: -->
+			<!-- currently the price and discount attribute is not compatible-->
+			<!-- in the above feature will result in some display error-->
+			{#each $rows as row}
+				<tr>
+					<td>{row.product_name}</td>
+					<td>
+						{#if row.discount !== 1}
+							<s>
+								{row.original_price}
+							</s>
+							<b>
+								only {row.selling_price}
+							</b>
+						{:else}
+							{row.selling_price}
+						{/if}
+					</td>
+					<td>{row.sales}</td>
+					<td>{row.likes}</td>
+					<td>{row.avg_score}</td>
+					<td>{row.tags}</td>
+				</tr>
+			{:else}
+				<tr>
+					<p>
+						Products not found!
+					</p>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+	<footer class="flex justify-between">
+		<RowCount {handler} />
+		<Pagination {handler} />
+	</footer>
+</div>
