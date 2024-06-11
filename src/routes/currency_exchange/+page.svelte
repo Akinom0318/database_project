647fbde5ff6a1d81e3b56a37e85b46be82bf89c7
Akinom <57698715+Akinom0318@users.noreply.github.com>
2024-06-11 22:13:47 +0800

    fix server.js (#29)

--- src/routes/currency_exchange/+page.svelte
+++ src/routes/currency_exchange/+page.svelte
@@ -1,5 +1,17 @@
 <script>
     import CurrencyExchange from "./CurrencyExchange.svelte";
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+    import { current_account_ID } from "../../store";
+
+    let local_current_account_ID = 0;
+    current_account_ID.subscribe((value) => {
+        local_current_account_ID = value;
+    })
+
+    if(!local_current_account_ID && browser){
+        goto('/');
+    }
 </script>
 
 <CurrencyExchange/>
