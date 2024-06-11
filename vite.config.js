d0c022d028b74333c0bd9e0299d6daf310d9d429
SunGj921028 <sungj921028@gmail.com>
2024-06-11 22:37:15 +0800

    Adjust package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	optimizeDeps: {
+		include: ['@skeletonlabs/skeleton', '@vincjo/datatables']
+	}
 });
