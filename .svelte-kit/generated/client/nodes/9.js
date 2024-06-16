387d27464b74339b48263cf2dbc896e2ce862ed2
Akinom <wuten770623@gmail.com>
2024-06-17 01:35:16 +0800

    thinner the code

--- .svelte-kit/generated/client/nodes/9.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/liking_list/+page.svelte";
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
+
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
 	optimizeDeps: {
 		include: ['@skeletonlabs/skeleton', '@vincjo/datatables']
-	},
+	}
 });
